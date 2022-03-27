c1639a0a0f855ac6df204c613de8a643822ef968
Cheyenne  Wells <cheyennewells@Cheyennes-Air.lan>
2022-03-27 13:24:12 -0500

    get users name from command line

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,14 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
+import "strings"
 
 func main() {
-	var name string = "Cheyenne"
-	fmt.Println("Hello, ", name)
-	name = "Jack"
-
-    fmt.Println("Hello, World!")
-	fmt.Println("Hello, ", name)
-	var age int = 24
-	fmt.Println("My age is", age)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your name")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSuffix(name, "\n")
+	fmt.Println("Hello", name)
 }
