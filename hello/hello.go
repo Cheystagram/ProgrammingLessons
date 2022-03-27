870d05eed0a75a51a59ad03fe3fb6a72ce1c9ad1
Cheyenne  Wells <cheyennewells@Cheyennes-Air.lan>
2022-03-27 12:45:19 -0500

    add hello.go

---
+++ hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Cheyenne"
+	fmt.Println("Hello, ", name)
+	name = "Jack"
+
+    fmt.Println("Hello, World!")
+	fmt.Println("Hello, ", name)
+	var age int = 24
+	fmt.Println("My age is", age)
+}
