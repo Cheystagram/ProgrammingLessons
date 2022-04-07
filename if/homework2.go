24b2b2424525fc58bcacd81aff950e31ba3d9a05
Jack Stenglein <jackstenglein@utexas.edu>
2022-04-07 10:24:51 -0500

    Add solution to homework2.go

--- if/homework2.go
+++ if/homework2.go
@@ -1,14 +1,38 @@
 package main
 
-// TODO: implement this function!
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 
 	// Create a bufio reader variable
+	reader := bufio.NewReader(os.Stdin)
 
 	// Print a message asking for the user's name and read in the response
+	fmt.Println("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSuffix(name, "\n")
 
 	// Use an if to print out the correct message
+	if name == "Cheyenne" {
+		fmt.Println("Wow, I'm the greatest!")
+	} else if name == "Jack" {
+		fmt.Println("You're the best teacher")
+	} else {
+		fmt.Println("I don't know you")
+	}
 
 	// Use a switch to print out the correct message
-
+	switch name {
+	case "Cheyenne":
+		fmt.Println("Wow, I'm the greatest!")
+	case "Jack":
+		fmt.Println("You're the best teacher")
+	default:
+		fmt.Println("I don't know you")
+	}
 }
