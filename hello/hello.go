d9f67bfeea3867c5197cfb6015e7a48b0e29c1b6
Cheyenne  Wells <cheyennewells@Cheyennes-Air.lan>
2022-04-06 20:59:23 -0500

    bye

--- hello/hello.go
+++ hello/hello.go
@@ -11,4 +11,20 @@ func main() {
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSuffix(name, "\n")
 	fmt.Println("Hello", name)
+
+	fmt.Println("Enter your age")
+	age, _ := reader.ReadString('\n')
+	age = strings.TrimSuffix(age, "\n")
+	fmt.Println("Hello", name, "you are", age)
+
+	fmt.Println("What is your job")
+	job, _ := reader.ReadString('\n')
+	job = strings.TrimSuffix(job, "\n")
+	fmt.Println("Hello", name, "you are", age, " and you are a", job)
+
+
+
+
+
+
 }
